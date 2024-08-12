450621ff489b1a3c144da43e58c8754381377ecb
Stefano Verna <s.verna@datocms.com>
2024-08-12 14:47:46 +0200

    Fix

---
+++ astro.config.mjs
@@ -0,0 +1,18 @@
+import { defineConfig, envField } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+  experimental: {
+    env: {
+      schema: {
+        DATOCMS_PUBLISHED_CONTENT_CDA_TOKEN: envField.string({
+          context: 'client',
+          access: 'public',
+        }),
+        DATOCMS_DRAFT_CONTENT_CDA_TOKEN: envField.string({ context: 'server', access: 'secret' }),
+        DATOCMS_CMA_TOKEN: envField.string({ context: 'server', access: 'secret' }),
+      },
+      validateSecrets: true,
+    },
+  },
+});
---
+++ src/lib/datocms/executeQuery.ts
@@ -0,0 +1,32 @@
+import { buildRequestInit, executeQuery as libExecuteQuery } from '@datocms/cda-client';
+import { DATOCMS_PUBLISHED_CONTENT_CDA_TOKEN } from 'astro:env/client';
+import { DATOCMS_DRAFT_CONTENT_CDA_TOKEN } from 'astro:env/server';
+import type { TadaDocumentNode } from 'gql.tada';
+
+export const cacheTag = 'datocms';
+
+/**
+ * Executes a GraphQL query using the DatoCMS Content Delivery API, and caches
+ * the result in Next.js Data Cache using the `cache: 'force-cache'` option.
+ */
+export async function executeQuery<Result, Variables>(
+  query: TadaDocumentNode<Result, Variables>,
+  options?: ExecuteQueryOptions<Variables>,
+) {
+  buildRequestInit;
+  const result = await libExecuteQuery(query, {
+    variables: options?.variables,
+    excludeInvalid: true,
+    includeDrafts: options?.includeDrafts,
+    token: options?.includeDrafts
+      ? DATOCMS_DRAFT_CONTENT_CDA_TOKEN
+      : DATOCMS_PUBLISHED_CONTENT_CDA_TOKEN,
+  });
+
+  return result;
+}
+
+type ExecuteQueryOptions<Variables> = {
+  variables?: Variables;
+  includeDrafts?: boolean;
+};
